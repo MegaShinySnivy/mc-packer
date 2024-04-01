ca403f2bf0af312f4f9c11adbc1f02c007a65a67
Maxine Alexander <25956545+MaxTheMooshroom@users.noreply.github.com>
2024-04-01 16:53:46 -0400

    updated binary elimination function

--- eliminate.py
+++ eliminate.py
@@ -5,41 +5,31 @@ from typing import List
 import random
 
 
-# find the only True value in the list
+# find the first True value in the list
 def binaryElimination(_list: List[bool]) -> int:
     left = 0
     right = len(_list) - 1
 
-    while left < right:
+    while left <= right:
         mid = (left + right) // 2
-        print(f'{left:04} -> {mid:04} -> {right:04}')
-        if any(_list[mid:right]):
-            left = mid + 1
-        else:
+        if any(_list[left:mid + 1]):
             right = mid - 1
+        else:
+            left = mid + 1
 
-    print('----------')
-    print(f'{left:04} -> {mid:04} -> {right:04} => {right}')
-    return right
+    return right + 1
 
 
 if __name__ == '__main__':
-    # for _ in [0]:
-    for _ in (range(2, 5000)):
-        # random_number = random.randint(1, _-1)
+    _list = [False] * 1024
+    for _ in tqdm(range(0, 5000000)):
         random_number = random.randint(0, 1023)
-        print(f'[{random_number}]')
-        # random_number = 50
-        # print(random_number)
-        _list = [False] * 1024
-        # print(_list)
         _list[random_number] = True
-
         result = binaryElimination(_list)
-        # print()
-        if result != random_number:
-            # print('Failure!!')
-            exit()
-        print('========================================')
 
+        if result != random_number:
+            print(f'Failure!! {result}')
+            exit()
+
+        _list[random_number] = False
     print('Success!!')
